c663459b26241e631c480e6a9152dd1fcb471a98
Stanley-Cao <ronaldcao28@gmail.com>
2026-04-10 19:14:09 -0400

    Initial commit

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!, 
+  rules: [
+    detectBot({
+      mode: "LIVE", 
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        "CATEGORY:MONITOR",
+        "CATEGORY:PREVIEW",
+        "STRIPE_WEBHOOK",
+      ],
+    }),
+  ],
+});
+
+async function authMiddleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
+};
+
+export default createMiddleware(aj, async (request: NextRequest) => {
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    return authMiddleware(request);
+  }
+
+  return NextResponse.next();
+});
