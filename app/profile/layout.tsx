6714daf17997e5f4b726afe653fdd1878ca0c58e
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2026-05-19 17:11:54 -0400

    Update AppSidebar import path in layout.tsx

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,7 +1,7 @@
 import { SiteHeader } from "@/components/sidebar/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { ReactNode } from "react";
-import { AppSidebar } from "@/app/dashboard/_components/DashboardAppSidebar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar"; 
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
