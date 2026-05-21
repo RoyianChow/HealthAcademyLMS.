d074a6f9ad33609baf91920bf2337f1eb3eec873
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2026-05-21 16:59:26 -0400

    Add Footer component to public layout

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { Navbar } from "@/app/(public)/_components/Navbar";
+import { Footer } from "@/app/(public)/_components/Footer";
 
 export default function LayoutPublic({ children }: { children: ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function LayoutPublic({ children }: { children: ReactNode }) {
       <main className="container mx-auto px-4 md:px-6 lg:px-8 mb-32">
         {children}
       </main>
+      <Footer />
     </div>
   );
 }
