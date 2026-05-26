179a36ef74af7569825c91fafb1d2028f8ad656b
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2026-05-26 17:11:37 -0400

    Update Editor.tsx

--- components/rich-text-editor/Editor.tsx
+++ components/rich-text-editor/Editor.tsx
@@ -5,6 +5,10 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Menubar } from "./Menubar";
 import TextAlign from "@tiptap/extension-text-align";
+import Table from "@tiptap/extension-table";
+import TableRow from "@tiptap/extension-table-row";
+import TableHeader from "@tiptap/extension-table-header";
+import TableCell from "@tiptap/extension-table-cell";
 
 export function RichTextEditor({ field }: { field: any }) {
   const editor = useEditor({
@@ -13,12 +17,18 @@ export function RichTextEditor({ field }: { field: any }) {
       TextAlign.configure({
         types: ["heading", "paragraph"],
       }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
     ],
 
     editorProps: {
       attributes: {
         class:
-          "min-h-[300px] p-4 focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-xl dark:prose-invert !w-full !max-w-none",
+          "min-h-[300px] p-4 focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-xl dark:prose-invert !w-full !max-w-none prose-table:border-collapse prose-td:border prose-th:border prose-th:bg-muted",
       },
     },
 
