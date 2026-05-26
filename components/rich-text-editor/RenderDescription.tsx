5a6ebccc7ef68068bbb3b0eaa41cea24e368022b
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2026-05-26 17:12:18 -0400

    Update RenderDescription.tsx

--- components/rich-text-editor/RenderDescription.tsx
+++ components/rich-text-editor/RenderDescription.tsx
@@ -5,6 +5,10 @@ import { generateHTML } from "@tiptap/html";
 import { type JSONContent } from "@tiptap/react";
 import TextAlign from "@tiptap/extension-text-align";
 import StarterKit from "@tiptap/starter-kit";
+import Table from "@tiptap/extension-table";
+import TableRow from "@tiptap/extension-table-row";
+import TableHeader from "@tiptap/extension-table-header";
+import TableCell from "@tiptap/extension-table-cell";
 import parse from "html-react-parser";
 
 export function RenderDescription({ json }: { json: JSONContent }) {
@@ -14,12 +18,20 @@ export function RenderDescription({ json }: { json: JSONContent }) {
       TextAlign.configure({
         types: ["heading", "paragraph"],
       }),
+      Table.configure({
+        resizable: false,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
     ]);
   }, [json]);
 
   return (
-    <div className="prose max-w-none w-full dark:prose-invert prose-li:marker:text-primary">
+  <div className="prose max-w-none w-full dark:prose-invert prose-li:marker:text-primary">
+    <div className="overflow-x-auto w-full">
       {parse(output)}
     </div>
-  );
+  </div>
+);
 }
