7187170401ac45f95924e37f8148a34ea7a753a7
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2026-05-26 22:03:51 -0400

    Add image support and update styling in RenderDescription
    Updated the RenderDescription component to include image support and adjusted the class names for better styling.

--- components/rich-text-editor/RenderDescription.tsx
+++ components/rich-text-editor/RenderDescription.tsx
@@ -1,3 +1,4 @@
+// components\rich-text-editor\RenderDescription.tsx
 "use client";
 
 import { useMemo } from "react";
@@ -9,12 +10,14 @@ import Table from "@tiptap/extension-table";
 import TableRow from "@tiptap/extension-table-row";
 import TableHeader from "@tiptap/extension-table-header";
 import TableCell from "@tiptap/extension-table-cell";
+import Image from "@tiptap/extension-image";
 import parse from "html-react-parser";
 
 export function RenderDescription({ json }: { json: JSONContent }) {
   const output = useMemo(() => {
     return generateHTML(json, [
       StarterKit,
+      Image,
       TextAlign.configure({
         types: ["heading", "paragraph"],
       }),
@@ -28,10 +31,10 @@ export function RenderDescription({ json }: { json: JSONContent }) {
   }, [json]);
 
   return (
-  <div className="prose max-w-none w-full dark:prose-invert prose-li:marker:text-primary">
-    <div className="overflow-x-auto w-full">
-      {parse(output)}
+    <div className="prose max-w-none w-full dark:prose-invert prose-li:marker:text-primary prose-img:rounded-md prose-img:mx-auto">
+      <div className="overflow-x-auto w-full">
+        {parse(output)}
+      </div>
     </div>
-  </div>
-);
+  );
 }
